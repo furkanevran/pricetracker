dc620152d30e0ba9c3f6c483b8afd947831c7aa0
furkanevran <evran@duck.com>
2022-08-30 18:53:00 +0300

    move UrlRequest to a new namespace

--- pricetracker.api/Program.cs
+++ pricetracker.api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using PriceTracker.API;
 using PriceTracker.Extractor;
 using PriceTracker.Infra;
 
--- pricetracker.api/UrlRequest.cs
+++ pricetracker.api/UrlRequest.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
+namespace PriceTracker.API;
+
 class UrlRequest
 {
     [JsonPropertyName("url"), Required] public string Url { get; set; } = null!;
