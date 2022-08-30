97ef0eb6176fbab88f1770bbabdf38a28520441d
furkanevran <evran@duck.com>
2022-08-30 17:57:35 +0300

    add extra line between field and method

--- pricetracker.extractor/Extractors/WatsonsExtractor.cs
+++ pricetracker.extractor/Extractors/WatsonsExtractor.cs
@@ -5,6 +5,7 @@ namespace PriceTracker.Extractor.Extractors;
 public class WatsonsExtractor : IExtractor
 {
     private const string URL = "https://www.watsons.com.tr/api/v2/wtctr/products/{0}";
+    
     public async Task<double> ExtractPrice(string url)
     {
         var html = await WebHelper.GetString(url);
