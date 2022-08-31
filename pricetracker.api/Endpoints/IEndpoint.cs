3bcddef06780d6798b72a6c490a7fad0ffc1d9fe
furkanevran <evran@duck.com>
2022-09-01 01:50:19 +0300

    remove DI remark at the IEndpoint

--- pricetracker.api/Endpoints/IEndpoint.cs
+++ pricetracker.api/Endpoints/IEndpoint.cs
@@ -2,7 +2,6 @@ namespace PriceTracker.API.Endpoints;
 
 /// <summary>
 /// Maps delegates to specified pattern.
-/// <remarks>Only singleton dependency injection should be used at the constructor.</remarks>
 /// </summary>
 public interface IEndpoint
 {
