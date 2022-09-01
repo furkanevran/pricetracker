e1c2c86a8375fe335d0e960c3c3152aee170be48
furkanevran <evran@duck.com>
2022-09-01 20:02:05 +0300

    tell JetBrains that IEndpoint interface is used implicitly

--- pricetracker.api/Endpoints/IEndpoint.cs
+++ pricetracker.api/Endpoints/IEndpoint.cs
@@ -1,8 +1,11 @@
+using JetBrains.Annotations;
+
 namespace PriceTracker.API.Endpoints;
 
 /// <summary>
 /// Maps delegates to specified pattern.
 /// </summary>
+[UsedImplicitly(ImplicitUseTargetFlags.WithInheritors | ImplicitUseTargetFlags.WithMembers)]
 public interface IEndpoint
 {
 }
