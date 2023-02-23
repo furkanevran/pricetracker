4b62d09fc1ca3cddad751eee09b067b85703c4e9
furkanevran <evran@duck.com>
2023-02-23 20:29:05 +0300

    make PasswordConfirm rules same with Password

--- pricetracker.api/Endpoints/Auth/RegisterRequest.cs
+++ pricetracker.api/Endpoints/Auth/RegisterRequest.cs
@@ -10,7 +10,8 @@ public class RegisterValidator : AbstractValidator<RegisterRequest>
     {
         RuleFor(x => x.Username).NotEmpty().MinimumLength(4).MaximumLength(32);
         RuleFor(x => x.EMail).NotEmpty().EmailAddress();
-        RuleFor(x => x.Password).NotEmpty();
-        RuleFor(x => x.PasswordConfirm).Equal(x => x.Password);
+
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
+        RuleFor(x => x.PasswordConfirm).NotEmpty().MinimumLength(8).Equal(x => x.Password);
     }
 }
